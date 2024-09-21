9311c7046e585f2ab86799364cf1de3a8e85ba30
azizktata <65952489+azizktata@users.noreply.github.com>
2024-09-21 09:54:09 +0100

    updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,48 +1,53 @@
-import React from "react"
-import { Link, NavLink } from "react-router-dom"
-import imageUrl from "/assets/images/avatar-icon.png"
+import React from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import imageUrl from "/assets/images/avatar-icon.png";
+import { IoIosLogOut } from "react-icons/io";
 
-export default function Header() {
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
-    }
+export default function Header({ isloggedin }) {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+  const navigate = useNavigate();
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+    navigate("/login", { replace: true });
+  }
 
-    function fakeLogOut() {
-        localStorage.removeItem("loggedin")
-    }
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
+      </Link>
+      <nav>
+        <NavLink
+          to="/host"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/vans"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Shop
+        </NavLink>
 
-    return (
-        <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
-                <NavLink
-                    to="/host"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
-                    Host
-                </NavLink>
-                <NavLink
-                    to="/about"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
-                    About
-                </NavLink>
-                <NavLink
-                    to="/vans"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
-                    Vans
-                </NavLink>
-                <Link to="login" className="login-link">
-                    <img
-                        src={imageUrl}
-                        className="login-icon"
-                    />
-                </Link>
-                <button onClick={fakeLogOut}>X</button>
-            </nav>
-        </header>
-    )
+        <Link to="login" className="login-link">
+          <img src={imageUrl} className="login-icon" />
+        </Link>
+
+        <button onClick={fakeLogOut}>
+          <IoIosLogOut />
+        </button>
+      </nav>
+    </header>
+  );
 }
